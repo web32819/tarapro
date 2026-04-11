c7633be55cfa8858ad33facc53d34123ce226697
web32819 <web@32bytes.com>
2026-04-11 13:29:21 +0530

    Fixed FontAwesome loading and optimized head

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,6 @@ import MobileNav from "./components/MobileNav";
 import Footer from "./components/Footer";
 import SetProductFromURLWrapper from "./components/SetProductFromURLWrapper";
 
-
 export const viewport = {
   width: "device-width",
   initialScale: 1,
@@ -26,157 +25,143 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning data-scroll-behavior="smooth">
-		<head>
-			<link rel="shortcut icon" href="/images/favicon.ico" />
+      <head>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
 
-  {/* Preconnect */}
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-  <link rel="preconnect" href="https://use.fontawesome.com" crossOrigin="anonymous" />
+        {/* Preconnect */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link rel="preconnect" href="https://use.fontawesome.com" crossOrigin="anonymous" />
 
-  {/* Combined Google Fonts - Single Request */}
-  <link
-    href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&family=Raleway:wght@300;400;600;700;900&display=swap"
-    rel="stylesheet"
-  />
+        {/* Combined Google Fonts - Single Request */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&family=Raleway:wght@300;400;600;700;900&display=swap"
+          rel="stylesheet"
+        />
 
-  {/* FontAwesome - Non Blocking */}
-  <link
-    rel="preload"
-    href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
-    as="style"
-    crossOrigin="anonymous"
-    onLoad={(e) => {
-      const link = e.currentTarget as HTMLLinkElement;
-      link.onload = null;
-      link.rel = "stylesheet";
-    }}
-  />
-  <noscript>
-    <link
-      rel="stylesheet"
-      href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
-      crossOrigin="anonymous"
-    />
-  </noscript>
+        {/* ✅ FontAwesome - loaded via Script instead of onLoad */}
+        <Script
+          id="fontawesome"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var link = document.createElement('link');
+              link.rel = 'stylesheet';
+              link.href = 'https://use.fontawesome.com/releases/v5.7.2/css/all.css';
+              link.crossOrigin = 'anonymous';
+              document.head.appendChild(link);
+            `,
+          }}
+        />
 
-  {/* Google Tag Manager */}
-  <Script id="gtm-script" strategy="afterInteractive">
-    {`
-      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-NRT2RJQ');
-    `}
-  </Script>
-			{/* End Google Tag Manager */}
-			
-		</head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NRT2RJQ');
+          `}
+        </Script>
+      </head>
 
-		<body>
-			{/* Google Tag Manager (noscript) */}
-			<noscript> <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NRT2RJQ" height="0" width="0" style={{ display: 'none', visibility: 'hidden' }} /></noscript>
-			{/* End Google Tag Manager (noscript) */}
-			
-			<Navbar />
-			<MobileNav />
-			
-			{/* ✅ Social Bar (ADD HERE) */}
-			<div className="social-bar visible-lg visible-md visible-sm">
-				<a href="https://www.facebook.com/TaraproFR" target="_blank" rel="noopener noreferrer" aria-label="Visit our Facebook page">
-				  <i className="fab fa-facebook-f"></i>
-				</a>
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NRT2RJQ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
 
-				<a href="https://www.linkedin.com/company/tara-lohia-pvt-ltd-/" target="_blank" rel="noopener noreferrer" aria-label="Visit our LinkedIn page">
-				  <i className="fab fa-linkedin-in"></i>
-				</a>
+        <Navbar />
+        <MobileNav />
 
-				<a href="https://www.instagram.com/taraprofr/" target="_blank" rel="noopener noreferrer" aria-label="Visit our Instagram page">
-				  <i className="fab fa-instagram"></i>
-				</a>
-			</div>
-			
+        {/* Social Bar */}
+        <div className="social-bar visible-lg visible-md visible-sm">
+          <a href="https://www.facebook.com/TaraproFR" target="_blank" rel="noopener noreferrer" aria-label="Visit our Facebook page">
+            <i className="fab fa-facebook-f"></i>
+          </a>
+          <a href="https://www.linkedin.com/company/tara-lohia-pvt-ltd-/" target="_blank" rel="noopener noreferrer" aria-label="Visit our LinkedIn page">
+            <i className="fab fa-linkedin-in"></i>
+          </a>
+          <a href="https://www.instagram.com/taraprofr/" target="_blank" rel="noopener noreferrer" aria-label="Visit our Instagram page">
+            <i className="fab fa-instagram"></i>
+          </a>
+        </div>
 
-			{/* Page content */}
-			{children}
-			
-			
-			<Footer/>
-			
-			<Script id="navbar-hover" strategy="afterInteractive">
-			  {`
-				$(window).on("load resize", function () {
-				  if (window.matchMedia("(min-width: 991px)").matches) {
-					$(".dropdown").hover(
-					  function () {
-						$(this).addClass("open");
-					  },
-					  function () {
-						$(this).removeClass("open");
-					  }
-					);
-				  }
-				});
-			  `}
-			</Script>
-			
-			{/* Sidebar */}
-			<Script id="sidebar-js" strategy="afterInteractive">
-			  {`
-				function openNav() {
-				  document.getElementById("mySidebar").style.width = "80%";
-				  document.getElementById("main").style.marginRight = "80%";
-				}
+        {children}
 
-				function closeNav() {
-				  document.getElementById("mySidebar").style.width = "0";
-				  document.getElementById("main").style.marginRight = "0";
-				}
-			  `}
-			</Script>
+        <Footer />
 
-			{/* Accordion */}
-			<Script id="accordion-js" strategy="afterInteractive">
-			  {`
-				var acc = document.getElementsByClassName("accordion");
-				for (var i = 0; i < acc.length; i++) {
-				  acc[i].addEventListener("click", function() {
-					this.classList.toggle("active");
-					var panel1 = this.nextElementSibling;
-					if (panel1.style.display === "block") {
-					  panel1.style.display = "none";
-					} else {
-					  panel1.style.display = "block";
-					}
-				  });
-				}
-			  `}
-			</Script>
+        {/* jQuery - load FIRST before other scripts */}
+        <Script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js" strategy="beforeInteractive" />
+        <Script src="/js/bootstrap.min.js" strategy="afterInteractive" />
+        <Script src="/js/owl.carousel.js" strategy="afterInteractive" />
 
-			<Script id="navbar-scroll" strategy="afterInteractive">
-			  {`
-				$(function() {
-				  var header = $(".navbar");
-				  $(window).scroll(function() {
-					var scroll = $(window).scrollTop();
-					if (scroll >= 50) {
-					  header.addClass("scrolled");
-					} else {
-					  header.removeClass("scrolled");
-					}
-				  });
-				});
-			  `}
-			</Script>
-			
+        <Script id="navbar-hover" strategy="afterInteractive">
+          {`
+            $(window).on("load resize", function () {
+              if (window.matchMedia("(min-width: 991px)").matches) {
+                $(".dropdown").hover(
+                  function () { $(this).addClass("open"); },
+                  function () { $(this).removeClass("open"); }
+                );
+              }
+            });
+          `}
+        </Script>
 
-			{/* Scripts */}
-			<Script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js" strategy="beforeInteractive" />
-			<Script src="/js/bootstrap.min.js" strategy="afterInteractive" />
-			<Script src="/js/owl.carousel.js" strategy="afterInteractive" />
+        <Script id="sidebar-js" strategy="afterInteractive">
+          {`
+            function openNav() {
+              document.getElementById("mySidebar").style.width = "80%";
+              document.getElementById("main").style.marginRight = "80%";
+            }
+            function closeNav() {
+              document.getElementById("mySidebar").style.width = "0";
+              document.getElementById("main").style.marginRight = "0";
+            }
+          `}
+        </Script>
 
-		</body>
+        <Script id="accordion-js" strategy="afterInteractive">
+          {`
+            var acc = document.getElementsByClassName("accordion");
+            for (var i = 0; i < acc.length; i++) {
+              acc[i].addEventListener("click", function() {
+                this.classList.toggle("active");
+                var panel1 = this.nextElementSibling;
+                if (panel1.style.display === "block") {
+                  panel1.style.display = "none";
+                } else {
+                  panel1.style.display = "block";
+                }
+              });
+            }
+          `}
+        </Script>
+
+        <Script id="navbar-scroll" strategy="afterInteractive">
+          {`
+            $(function() {
+              var header = $(".navbar");
+              $(window).scroll(function() {
+                var scroll = $(window).scrollTop();
+                if (scroll >= 50) {
+                  header.addClass("scrolled");
+                } else {
+                  header.removeClass("scrolled");
+                }
+              });
+            });
+          `}
+        </Script>
+
+      </body>
     </html>
   );
 }
